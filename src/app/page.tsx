f18c2887a8e0252e5763d2ccd1d78e3a1ccf0595
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-11-08 02:39:04 +0900

    動的なファルダーパターンを作成した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Next.js",
+};
+
 export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+  return <Link href="/blog">blog</Link>;
 }
